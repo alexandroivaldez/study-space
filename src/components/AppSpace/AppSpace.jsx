7caa16f0fef17cb1ee6ad16081c1db88f26b595a
Alexandro Valdez <alexandroivaldez@gmail.com>
2023-05-13 16:43:50 -0500

    Delegated space for sideBar and studySpace

---
+++ src/components/AppSpace/AppSpace.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./AppSpace.css"
+
+function AppSpace() {
+  return (
+    <div className='appSpace-container'>AppSpace</div>
+  )
+}
+
+export default AppSpace
---
+++ src/components/SideMenu/SideMenu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./SideMenu.css"
+
+function SideMenu() {
+  return (
+    <div className='sideMenu-container'>SideMenu</div>
+  )
+}
+
+export default SideMenu
