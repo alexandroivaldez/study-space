953f2d5cbe1baf3d0b5c8e3a586f2a8c047de359
Alexandro Valdez <alexandroivaldez@gmail.com>
2023-05-13 22:59:27 -0500

    Added youtube prototype component

---
+++ src/components/YouTubeBox/YouTubeBox.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+function YouTubeBox({ initialPosition }) {
+  const [position, setPosition] = useState(initialPosition);
+  const [isDragging, setIsDragging] = useState(false);
+  const boxRef = useRef(null);
+
+  useEffect(() => {
+    const handleMouseUp = () => {
+      if (isDragging) {
+        setIsDragging(false);
+      }
+    };
+
+    window.addEventListener('mouseup', handleMouseUp);
+
+    return () => {
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, [isDragging]);
+
+  const handleMouseDown = (event) => {
+    if (boxRef.current.contains(event.target)) {
+      setIsDragging(true);
+    }
+  };
+
+  const handleMouseMove = (event) => {
+    if (isDragging) {
+      const appRect = document.body.getBoundingClientRect();
+      const containerRect = boxRef.current.parentNode.getBoundingClientRect();
+      const boxRect = boxRef.current.getBoundingClientRect();
+      const maxX = containerRect.width - boxRect.width;
+      const maxY = appRect.height - boxRect.height;
+      let newX = position.x + event.movementX;
+      let newY = position.y + event.movementY;
+      newX = Math.min(Math.max(newX, 0), maxX);
+      newY = Math.min(Math.max(newY, 0), maxY);
+      const newPosition = { x: newX, y: newY };
+      setPosition(newPosition);
+    }
+  };
+  
+  const boxStyle = {
+    position: 'absolute',
+    left: position.x + 'px',
+    top: position.y + 'px',
+    borderRadius: '15px',
+    width: '500px',
+    height: '350px',
+    backgroundColor: 'red',
+    cursor: isDragging ? 'move' : 'default',
+  };
+
+  return (
+    <div
+      className="box"
+      ref={boxRef}
+      style={boxStyle}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+    >
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/d9YM_9CVmtc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+  );
+}
+
+export default YouTubeBox;
