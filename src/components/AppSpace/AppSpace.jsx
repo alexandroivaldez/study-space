d4a1f8dff7cc65f42d0808289b837b396746724b
Alexandro Valdez <alexandroivaldez@gmail.com>
2023-05-20 20:49:33 -0500

    Added StickyNoteContainer

--- src/components/AppSpace/AppSpace.jsx
+++ src/components/AppSpace/AppSpace.jsx
@@ -5,6 +5,7 @@ import "./AppSpace.css"
 import ColorChanger from '../ColorChanger/ColorChanger';
 import SideMenu from '../SideMenu/SideMenu';
 import PomodoroTimer from '../PomodoroTimer/PomodoroTimer';
+import StickyNoteContainer from '../StickyNoteContainer/StickyNoteContainer';
 
 function AppSpace(props) {
 
@@ -30,6 +31,7 @@ function AppSpace(props) {
         isActive={isActive} setActive={setActive}
         />
       </div>
+      <StickyNoteContainer initialPosition={{ x: 500, y: 200 }} /> 
       {/* <Box initialPosition={{ x: 500, y: 200 }} /> */}
     </div>
   );
